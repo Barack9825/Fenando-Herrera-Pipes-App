55887db56fc14fcde2d889028ece0ba7494dd825
Ruben David <ruben@jobs2live.com>
2024-05-08 10:22:36 -0400

    Fernando Herrera Sec-10 014 PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AppComponent } from './app.component';
 //Modulo personalizado
 import { AppRouterModule } from './app-router.module';
 import { SharedModule } from './shared/shared.module';
-
-
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,AppRouterModule,SharedModule],
+  imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +22,6 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     NumerosComponent,
     OrdenarComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule,PrimeNgModule],
 })
 export class VentasModule {}
