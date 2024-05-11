03a144bd2592fc2d44a225727bce0ad382a80559
Ruben David <ruben@jobs2live.com>
2024-05-11 14:19:03 -0400

    Fernando Herrera Sec-10 023 i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -5,11 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './no-comunes.component.html',
 })
 export class NoComunesComponent {
+  //i18nSelect
   nombre: string = 'Fernando';
   genero: string = 'masculino';
 
   invitacionMap = {
-    'masculino': 'invitarlo',
-    'femenino':'invitarla'
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+  };
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    'other': 'tenemos # clientes esperando'
   };
 }
