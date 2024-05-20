650aae600ff5f1d5c7e29f9e91b13980d86fca65
Ruben David <ruben@jobs2live.com>
2024-05-19 23:53:22 -0400

    Fernando Herrera Sec-11 010 Parametrizar nuestro pipe personalizado

--- src/app/ventas/pipes/ordenar.pipe.ts
+++ src/app/ventas/pipes/ordenar.pipe.ts
@@ -5,9 +5,22 @@ import { Heroe } from '../interfaces/ventas.interfaces';
   name: 'ordenar',
 })
 export class OrdenarPipe implements PipeTransform {
-  transform(heroes: Heroe[]): Heroe[] {
-    return heroes.sort((heroeA, heroeB) =>
-      heroeA.nombre > heroeB.nombre ? 1 : -1
-    );
+  transform(heroes: Heroe[], orderPor: string = 'sin valor'): Heroe[] {
+    switch (orderPor) {
+      case 'nombre':
+        return heroes.sort((heroeA, heroeB) =>
+          heroeA.nombre > heroeB.nombre ? 1 : -1
+        );
+      case 'vuela':
+         return heroes.sort((heroeA, heroeB) =>
+          heroeA.vuela > heroeB.vuela ? -1 : 1
+        );
+      case 'color':
+         return heroes.sort((heroeA, heroeB) =>
+           heroeA.color > heroeB.color ? 1 : -1
+         );
+      default:
+        return heroes;
+    }
   }
 }
