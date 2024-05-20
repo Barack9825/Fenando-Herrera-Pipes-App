4cab7c5314cb2316e417c136680c8c507889e47a
Ruben David <ruben@jobs2live.com>
2024-05-19 23:10:09 -0400

    Fernando Herrera Sec-11 07 Llenar un PrimeTable con data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
   enMayusculas: boolean = true;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
+
   toggleMayusculas(): void {
     this.enMayusculas = !this.enMayusculas;
   }
